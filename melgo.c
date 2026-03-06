4d22c0dc40eb123c83efa4992a82efd982ea260b
Martin Brignall <martin.brignall@OVO-LPXDY5HKQ2.local>
2026-03-06 10:21:12 +0000

    End of chapter 6
    Incremental search with arrows (for next/previous), enter to goto
    cursor and escape to exit.

--- melgo.c
+++ melgo.c
@@ -72,7 +72,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen(void);
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
 /*** terminal ***/
 
@@ -169,6 +169,18 @@ int editorRowCxToRx(erow *row, int cx) {
     rx++;
   }
   return rx;
+}
+
+int editorRowRxToCx(erow *row, int rx) {
+  int cur_rx = 0;
+  int cx;
+  for (cx = 0; cx < row->size; cx++) {
+    if (row->chars[cx] == '\t')
+      cur_rx += (MELGO_TAB_STOP - 1) - (cur_rx % MELGO_TAB_STOP);
+    cur_rx++;
+    if (cur_rx > rx) return cx;
+  }
+  return cx;
 }
 
 void editorUpdateRow(erow *row) {
@@ -366,14 +378,14 @@ void editorOpen(char *filename) {
   }
   free(line);
   fclose(fp);
-  E.dirty = 0; 
+  E.dirty = 0;
 }
 
 void editorSave(void) {
   if (E.filename == NULL) {
-    E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+    E.filename = editorPrompt("save as: %s (esc to cancel)", NULL);
     if (E.filename == NULL) {
-      editorSetStatusMessage("Save aborted");
+      editorSetStatusMessage("save aborted");
       return;
     }
   }
@@ -396,7 +408,65 @@ void editorSave(void) {
     close(fd);
   }
   free(buf);
-  editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+  editorSetStatusMessage("cannot save - I/O error: %s", strerror(errno));
+}
+
+/*** find ***/
+
+void editorFindCallback(char *query, int key) {
+  static int last_match = -1;
+  static int direction = 1;
+
+  if (key == '\r' || key == '\x1b') {
+    last_match = -1;
+    direction = 1;
+    return;
+  } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+    direction = 1;
+  } else if (key == ARROW_LEFT || key == ARROW_UP) {
+    direction = -1;
+  } else {
+    last_match = -1;
+    direction = 1;
+  }
+
+  if (last_match == -1) direction = 1;
+  int current = last_match;
+
+  int i;
+  for (i = 0; i < E.numrows; i++) {
+    current += direction;
+    if (current == -1) current = E.numrows - 1;
+    else if (current == E.numrows) current = 0;
+
+    erow *row = &E.row[current];
+    char *match = strstr(row->render, query);
+    if (match) {
+      last_match = current;
+      E.cy = current;
+      E.cx = editorRowRxToCx(row, match - row->render);
+      E.rowoff = E.numrows;
+      break;
+    }
+  }
+}
+
+void editorFind(void) {
+  int saved_cx = E.cx;
+  int saved_cy = E.cy;
+  int saved_coloff = E.coloff;
+  int saved_rowoff = E.rowoff;
+
+  char *query = editorPrompt("search: %s (esc/arrows/enter)", editorFindCallback);
+  
+  if (query) {
+    free(query);
+  } else {
+    E.cx = saved_cx;
+    E.cy = saved_cy;
+    E.coloff = saved_coloff;
+    E.rowoff = saved_rowoff;
+  }
 }
 
 /*** append buffer ***/
@@ -456,7 +526,7 @@ void editorDrawRows(struct abuf *ab) {
       if (E.numrows == 0 && y == E.screenrows / 3) {
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome),
-        "*** melgo editor *** version %s ***", MELGO_VERSION);
+        "melgo editor -- version %s", MELGO_VERSION);
         if (welcomelen > E.screencols) welcomelen = E.screencols;
         int padding = (E.screencols - welcomelen) / 2;
         if (padding) {
@@ -483,8 +553,8 @@ void editorDrawRows(struct abuf *ab) {
 void editorDrawStatusBar(struct abuf *ab) {
   abAppend(ab, "\x1b[7m", 4);
   char status[80], rstatus[80];
-  int len = snprintf(status, sizeof(status), " %.20s - %d lines %s",
-    E.filename ? E.filename : "[No Name]", E.numrows,
+  int len = snprintf(status, sizeof(status), " %.20s - %d ln %s",
+    E.filename ? E.filename : "[no name]", E.numrows,
     E.dirty ? "(modified)" : "");
   int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d ",
     E.cy + 1, E.numrows);
@@ -546,7 +616,7 @@ void editorSetStatusMessage(const char *fmt, ...) {
 
 /*** input ***/
 
-char *editorPrompt(char *prompt) {
+char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
   size_t bufsize = 128;
   char *buf = malloc(bufsize);
 
@@ -562,11 +632,13 @@ char *editorPrompt(char *prompt) {
       if (buflen != 0) buf[--buflen] = '\0';
     } else if (c == '\x1b') {
       editorSetStatusMessage("");
+      if (callback) callback(buf, c),
       free(buf);
       return NULL;
     } else if (c == '\r') {
       if (buflen != 0) {
         editorSetStatusMessage("");
+        if (callback) callback(buf, c);
         return buf;
       }
     } else if (!iscntrl(c) && c < 128) {
@@ -577,6 +649,7 @@ char *editorPrompt(char *prompt) {
       buf[buflen++] = c;
       buf[buflen] = '\0';
     }
+      if (callback) callback(buf, c);
   }
 }
 
@@ -635,8 +708,8 @@ void editorProcessKeypress(void) {
 
   case CTRL_KEY('q'):
     if (E.dirty && quit_times > 0) {
-      editorSetStatusMessage("File has unsaved changes! "
-        "| Press ctrl-q %d more times to quit", quit_times);
+      editorSetStatusMessage("file has unsaved changes "
+        "| press ctrl-q %d more times to quit", quit_times);
       quit_times--;
       return;
     }
@@ -656,6 +729,10 @@ void editorProcessKeypress(void) {
   case END_KEY:
     if (E.cy < E.numrows)
       E.cx = E.row[E.cy].size;
+    break;
+
+  case CTRL_KEY('f'):
+    editorFind();
     break;
 
   case BACKSPACE:
@@ -729,7 +806,8 @@ int main(int argc, char *argv[]) {
     editorOpen(argv[1]);
   }
 
-  editorSetStatusMessage(" helper: ctrl-s for save | ctrl-q for quit");
+  editorSetStatusMessage(
+   "helper: ctrl-s to save | ctrl-q to quit | ctrl-f to find");
 
   while (1) {
     editorRefreshScreen();
